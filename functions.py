fae58ff4f0aa7a5715e3b26d98d7a85d2b252868
leo26k <leoklaric2004@gmail.com>
2024-08-31 19:55:28 +0200

    - added players and region database - expaned the basic build command

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 import json
 
 
+players=['test_player_1', 'test_player_2', 'test_player_3', 'test_player_4', 'test_player_5']
+
 def check_roles(ctx):
 	returning=[False, False, None]
 	for r in ctx.author.roles:
@@ -8,7 +10,7 @@ def check_roles(ctx):
 			returning[0]=True
 		elif str(r).lower() == 'player':
 			returning[1]=True
-		elif str(r).lower() in nations['nations']:
+		elif str(r).lower() in players:
 			returning[2]=str(r).lower()
 	print(returning)
 	return returning
@@ -20,3 +22,9 @@ def read_db():
 		json_object = json.load(openfile)
 		openfile.close()
 	return json_object
+
+def write_db(db):
+	with open('players.json', 'w') as openfile:
+		db=json.dumps(db, indent=4)
+		openfile.write(db)
+		openfile.close()
