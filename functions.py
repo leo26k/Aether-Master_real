ad03b48432548e87eb3abd8ec0d407f8ce1f435d
leo26k <leoklaric2004@gmail.com>
2024-09-19 20:39:57 +0200

    Added a config file for roles and the upgrade command

--- functions.py
+++ functions.py
@@ -3,14 +3,22 @@ import json
 
 players=['test_player_1', 'test_player_2', 'test_player_3', 'test_player_4', 'test_player_5']
 
-def check_roles(ctx):
+def load_config():
+	with open('config.json', 'r') as openfile:
+	# Reading from json file
+		json_object = json.load(openfile)
+		openfile.close()
+	return json_object
+
+
+def check_roles(ctx, config):
 	returning=[False, False, None]
 	for r in ctx.author.roles:
-		if str(r).lower() == 'game master man':
+		if str(r).lower() == config['game_master_role']:
 			returning[0]=True
-		elif str(r).lower() == 'player':
+		elif str(r).lower() == config['player_role']:
 			returning[1]=True
-		elif str(r).lower() in players:
+		elif str(r).lower() in config['all_player_roles']:
 			returning[2]=str(r).lower()
 	print(returning)
 	return returning
